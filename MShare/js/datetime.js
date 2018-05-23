b543cb63daff6b82b38ff76d52b20b665c08e9c9
WangZhenyu <847315251@qq.com>
2018-05-23 13:46:37 +0800

    更新datetime.js中的变量名

--- MShare/js/datetime.js
+++ MShare/js/datetime.js
@@ -2,13 +2,13 @@ function timestampToDatetime(value) {
     if (value === undefined || value === null || value === '') {
         return '-';
     } else {
-        let date = new Date(value);
-        let year = date.getFullYear();
-        let month = date.getMonth() + 1;
-        let day = date.getDate();
-        let hour = date.getHours();
-        let minute = date.getMinutes();
-        let second = date.getSeconds();
+        let currDate = new Date(value);
+        let year = currDate.getFullYear();
+        let month = currDate.getMonth() + 1;
+        let day = currDate.getDate();
+        let hour = currDate.getHours();
+        let minute = currDate.getMinutes();
+        let second = currDate.getSeconds();
         month = month < 10 ? '0' + month : month;
         day = day < 10 ? '0' + day : day;
         hour = hour < 10 ? '0' + hour : hour;
