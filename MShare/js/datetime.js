1c5e2b287868bd459d5b23c4a95bb08991a2b81d
WangZhenyu <847315251@qq.com>
2018-05-23 14:00:45 +0800

    let关键字修改成var

--- MShare/js/datetime.js
+++ MShare/js/datetime.js
@@ -2,13 +2,13 @@ function timestampToDatetime(value) {
     if (value === undefined || value === null || value === '') {
         return '-';
     } else {
-        let currDate = new Date(value);
-        let year = currDate.getFullYear();
-        let month = currDate.getMonth() + 1;
-        let day = currDate.getDate();
-        let hour = currDate.getHours();
-        let minute = currDate.getMinutes();
-        let second = currDate.getSeconds();
+        var currDate = new Date(value);
+        var year = currDate.getFullYear();
+        var month = currDate.getMonth() + 1;
+        var day = currDate.getDate();
+        var hour = currDate.getHours();
+        var minute = currDate.getMinutes();
+        var second = currDate.getSeconds();
         month = month < 10 ? '0' + month : month;
         day = day < 10 ? '0' + day : day;
         hour = hour < 10 ? '0' + hour : hour;
